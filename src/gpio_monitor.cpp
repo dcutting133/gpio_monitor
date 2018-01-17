1a27f11db781039a82b6c148b79c197aa61e4c7e
dcutting133 <dcutting133@github.com>
2018-01-17 22:05:02 +0000

    Cut down the program to barebones, fixed cmake, and fixed messages. Now buildable.

--- src/gpio_monitor.cpp
+++ src/gpio_monitor.cpp
@@ -1,7 +1,7 @@
 #include "ros/ros.h"
 #include "wiringPi.h"
-#include "yeti_snowplow/gpio_inputs.h"
-#include "yeti_snowplow/gpio_leds.h"
+#include "gpio_monitor/gpio_input.h"
+#include "gpio_monitor/gpio_output.h"
 
 /**
  * RASPBERRY PI 3B PINS
@@ -10,61 +10,45 @@
  * 
  * https://www.raspberrypi.org/documentation/usage/gpio-plus-and-raspi2/
  */
-#define ESTOP 2
-#define BUTTON1 3
-#define SWITCH1 4
 
-#define LED1 14 // meant for drive mode, can change name later
+#define INPUT1 0 // (pin 17 / phys pin 11), ESTOP
+#define INPUT2 2 // (pin 27 / phys pin 13), NA
+#define INPUT3 3 // (pin 22 / phys pin 15), NA
 
-yeti_snowplow::gpio_inputs inputs;
-bool estop;
-bool button1;
-bool switch1;
+#define OUTPUT1 1 // (pin 18 / phys pin 12), drive mode led
+#define OUTPUT2 4 // (pin 23 / phys pin 16), NA
+#define OUTPUT3 5 // (pin 24 / phys pin 18), NA
 
-yeti_snowplow::gpio_leds leds;
-bool led1;
+gpio_monitor::gpio_input inputs;
+bool input1;
+
+gpio_monitor::gpio_output outputs;
+bool output1;
 
 ros::Publisher gpioPub;
 
 void initPins() {
-    pinMode(ESTOP, INPUT);
-    pinMode(BUTTON1, INPUT);
-    pinMode(SWITCH1, INPUT);
-
-    pinMode(LED1, OUTPUT);
+    pinMode(INPUT1, INPUT);
+    pinMode(OUTPUT1, OUTPUT);
 }
 
-void updateInputs() {
-    estop = digitalRead(ESTOP) == 0 ? false : true;
-    button1 = digitalRead(BUTTON1) == 0 ? false : true;
-    switch1 = digitalRead(SWITCH1) == 0 ? false : true;
-    inputs.estop = estop;
-    inputs.button1 = button1;
-    inputs.switch1 = switch1;
-    gpioPub.publish(inputs);
+void updateOutputs() {
+
 }
 
-void updateLeds() {
-    if(led1) {
-        digitalWrite(LED1, 1);
-    }
-}
-
-void ledCallback(const yeti_snowplow::gpio_leds::ConstPtr& leds) {
-    led1 = leds->led1;
-    updateLeds();
+void outputCallback(const gpio_monitor::gpio_output::ConstPtr& outputs) {
+    output1 = outputs->driveMode;
+    updateOutputs();
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "gpio_monitor");
     ros::NodeHandle n;
     wiringPiSetup(); // Enables WiringPi for GPIO control
-
     initPins();
-    updateLeds();
-    ros::Subscriber gpioSub = n.subscribe("gpio/leds", 5, ledCallback);
-    gpioPub = n.advertise<yeti_snowplow::gpio_inputs>("gpio/inputs", 5);
-    updateInputs();
+
+    ros::Subscriber gpioSub = n.subscribe("gpio/outputs", 5, outputCallback);
+    gpioPub = n.advertise<gpio_monitor::gpio_input>("gpio/inputs", 5);
     ros::spin();
     return 0;
 }
